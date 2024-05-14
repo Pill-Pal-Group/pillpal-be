ed7a8011b7a3112d4eb1162b9277ff24d0f82d0c
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-05-15 06:32:26 +0700

    feat: drug related services

---
+++ src/PillPal.Service/Applications/Ingredients/IIngredientService.cs
@@ -0,0 +1,10 @@
+﻿using PillPal.Core.Dtos.Ingredients.Commands;
+using PillPal.Core.Dtos.Ingredients.Queries;
+
+namespace PillPal.Service.Applications.Ingredients;
+
+public interface IIngredientService
+{
+    Task<IEnumerable<GetIngredientQuery>> GetIngredientsAsync();
+    Task CreateIngredientAsync(CreateIngredientCommand ingredient);
+}
---
+++ src/PillPal.WebApi/Controllers/DrugsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PillPal.Core.Dtos.Drugs.Commands;
+using PillPal.Service.Applications.Drugs;
+
+namespace PillPal.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DrugsController : ControllerBase
+{
+    private readonly IDrugService _drugService;
+
+    public DrugsController(IDrugService drugService)
+    {
+        _drugService = drugService;
+    }
+
+    [HttpGet(Name = "Get Drug List")]
+    public async Task<IActionResult> GetDrugList()
+    {
+        return Ok(await _drugService.GetDrugsAsync());
+    }
+
+    [HttpPost(Name = "Add Drug")]
+    public async Task<IActionResult> PostDrug([FromBody] CreateDrugCommand drug)
+    {
+        await _drugService.CreateDrugAsync(drug);
+        return Ok();
+    }
+}
