d9aedd403168dee056b239635e3d2998045c6417
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-05-28 02:47:19 +0700

    fix: not found exception

--- src/PillPal.Application/Common/Exceptions/NotFoundException.cs
+++ src/PillPal.Application/Common/Exceptions/NotFoundException.cs
@@ -1,6 +1,7 @@
-﻿namespace PillPal.Application.Common.Exceptions;
+﻿
+namespace PillPal.Application.Common.Exceptions;
 
-public class NotFoundException(string message)
-    : HttpException(message, HttpStatusCode.NotFound)
+public class NotFoundException(string entityName, object key)
+    : HttpException($"Entity '{entityName}' with identifier ({key}) was not found.", HttpStatusCode.NotFound)
 {
 }
