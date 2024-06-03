3cd4a9c023a8ec810f8190acc5d54df2900141a4
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-06-03 20:00:03 +0700

    feat: update test auth controller

--- src/PillPal.WebApi/Controllers/TestAuthsController.cs
+++ src/PillPal.WebApi/Controllers/TestAuthsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using PillPal.Core.Constant;
 using PillPal.WebApi.Service;
 
@@ -8,6 +9,9 @@ namespace PillPal.WebApi.Controllers;
 [ApiController]
 public class TestAuthsController : ControllerBase
 {
+    /// <summary>
+    /// Allow only admin role
+    /// </summary>
     [HttpGet("admin-only")]
     [AuthorizeRoles(Role.Admin)]
     public string TestAdmin()
@@ -15,6 +19,9 @@ public class TestAuthsController : ControllerBase
         return "admin verified";
     }
 
+    /// <summary>
+    /// Allow only customer role
+    /// </summary>
     [HttpGet("customer-only")]
     [AuthorizeRoles(Role.Customer)]
     public string TestCustomer()
@@ -22,10 +29,53 @@ public class TestAuthsController : ControllerBase
         return "customer verified";
     }
 
+    /// <summary>
+    /// Allow only manager role
+    /// </summary>
+    [HttpGet("manager-only")]
+    [AuthorizeRoles(Role.Manager)]
+    public string TestManager()
+    {
+        return "manager verified";
+    }
+
+    /// <summary>
+    /// Allow both admin and customer role
+    /// </summary>
     [HttpGet("both-admin-customer")]
     [AuthorizeRoles(Role.Admin, Role.Customer)]
-    public string TestBoth()
+    public string TestAdminCus()
     {
         return "both admin and customer allow";
+    }
+
+    /// <summary>
+    /// Allow both admin and manager role
+    /// </summary>
+    [HttpGet("both-admin-manager")]
+    [AuthorizeRoles(Role.Admin, Role.Manager)]
+    public string TestAdminMng()
+    {
+        return "both admin and manager allow";
+    }
+
+    /// <summary>
+    /// Allow all roles admin, manager and customer
+    /// </summary>
+    [HttpGet("all-three")]
+    [AuthorizeRoles(Role.Admin, Role.Manager, Role.Customer)]
+    public string TestAllThreee()
+    {
+        return "all roles admin, manager and customer allow";
+    }
+
+    /// <summary>
+    /// Allow annonymous user (non-login user)
+    /// </summary>
+    [HttpGet("annonymous")]
+    [AllowAnonymous]
+    public string TestBoth()
+    {
+        return "non-login user allow";
     }
 }
