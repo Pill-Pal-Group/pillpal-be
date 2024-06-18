2bd4f41d76b0806c7c885507a777db3fe8aa8836
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-06-18 16:51:46 +0700

    chore: add editorconfig and clean code

--- src/PillPal.Infrastructure/Auth/JwtService.cs
+++ src/PillPal.Infrastructure/Auth/JwtService.cs
@@ -6,87 +6,81 @@ using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace PillPal.Infrastructure.Auth
+namespace PillPal.Infrastructure.Auth;
+
+public class JwtService : IJwtService
 {
-    public class JwtService : IJwtService
+    private readonly JwtSettings _settings;
+    private readonly JwtSecurityTokenHandler _tokenHandler;
+
+    public JwtService(IOptions<JwtSettings> settings)
     {
-        private readonly JwtSettings _settings;
-        private readonly JwtSecurityTokenHandler _tokenHandler;
+        _settings = settings.Value;
+        _tokenHandler = new();
+    }
 
-        public JwtService(IOptions<JwtSettings> settings)
+    public (string accessToken, int expired) GenerateJwtToken(ApplicationUser user, string role)
+    {
+        var signingCredential = new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_settings.SecretKey!)), SecurityAlgorithms.HmacSha256Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
         {
-            _settings = settings.Value;
-            _tokenHandler = new();
+            Subject = new ClaimsIdentity(
+            [
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email!),
+                new Claim(ClaimTypes.Name, user.UserName!),
+                new Claim(ClaimTypes.Role, role)
+            ]),
+            IssuedAt = DateTime.UtcNow,
+            Expires = DateTime.UtcNow.AddMinutes(_settings.Expires),
+            Issuer = _settings.Issuer,
+            Audience = _settings.Audience,
+            SigningCredentials = signingCredential
+        };
+
+        var token = _tokenHandler.CreateToken(tokenDescriptor);
+
+        return (_tokenHandler.WriteToken(token), (int)_settings.Expires);
+    }
+
+    public string GenerateRefreshToken(string token)
+    {
+        var hash = SHA256.HashData(Encoding.ASCII.GetBytes(token));
+
+        return Convert.ToBase64String(hash);
+    }
+
+    public bool ValidateRefreshToken(string token, string refreshToken)
+    {
+        var hash = SHA256.HashData(Encoding.ASCII.GetBytes(token));
+
+        return refreshToken == Convert.ToBase64String(hash);
+    }
+
+    public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+    {
+        var TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = _settings.Issuer,
+            ValidAudience = _settings.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_settings.SecretKey!))
+        };
+
+        var principal = _tokenHandler.ValidateToken(token, TokenValidationParameters, out SecurityToken securityToken);
+
+        var jwtToken = securityToken as JwtSecurityToken;
+
+        if (jwtToken is null || !jwtToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+        {
+            throw new SecurityTokenException("Invalid token");
         }
 
-        public (string accessToken, int expired) GenerateJwtToken(ApplicationUser user, string role)
-        {
-            var signingCredential = new SigningCredentials(
-                new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_settings.SecretKey!)), SecurityAlgorithms.HmacSha256Signature);
-
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(
-                [
-                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                    new Claim(ClaimTypes.Email, user.Email!),
-                    new Claim(ClaimTypes.Name, user.UserName!),
-                    new Claim(ClaimTypes.Role, role)
-                ]),
-                IssuedAt = DateTime.UtcNow,
-                Expires = DateTime.UtcNow.AddMinutes(_settings.Expires),
-                Issuer = _settings.Issuer,
-                Audience = _settings.Audience,
-                SigningCredentials = signingCredential
-            };
-
-            var token = _tokenHandler.CreateToken(tokenDescriptor);
-
-            return (_tokenHandler.WriteToken(token), (int)_settings.Expires);
-        }
-
-        public string GenerateRefreshToken(string token)
-        {
-            using var sha256 = SHA256.Create();
-
-            var hash = sha256.ComputeHash(Encoding.ASCII.GetBytes(token));
-
-            return Convert.ToBase64String(hash);
-        }
-
-        public bool ValidateRefreshToken(string token, string refreshToken)
-        {
-            using var sha256 = SHA256.Create();
-
-            var hash = sha256.ComputeHash(Encoding.ASCII.GetBytes(token));
-
-            return refreshToken == Convert.ToBase64String(hash);
-        }
-
-        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
-        {
-            var TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = _settings.Issuer,
-                ValidAudience = _settings.Audience,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_settings.SecretKey!))
-            };
-
-            SecurityToken securityToken;
-            var principal = _tokenHandler.ValidateToken(token, TokenValidationParameters, out securityToken);
-
-            var jwtToken = securityToken as JwtSecurityToken;
-
-            if (jwtToken is null || !jwtToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
-            {
-                throw new SecurityTokenException("Invalid token");
-            }
-
-            return principal;
-        }
+        return principal;
     }
 }
