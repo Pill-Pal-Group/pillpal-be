dd4f1be555d5dc954541edef2bbf92905b28cc72
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-07-13 23:10:29 +0700

    chore: add appsettings example and minor refactor

--- src/PillPal.WebApi/Service/UserAccessor.cs
+++ src/PillPal.WebApi/Service/UserAccessor.cs
@@ -5,6 +5,6 @@ namespace PillPal.WebApi.Service;
 
 public class UserAccessor(IHttpContextAccessor accessor) : IUser
 {
-    public string? Id =>
-        accessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier) ?? "Manual";
+    public string? Id
+        => accessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier) ?? "Manual";
 }
