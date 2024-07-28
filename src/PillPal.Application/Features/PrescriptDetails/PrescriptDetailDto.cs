5641b0cf6758acc79efade13632525f109bad7cb
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-07-28 14:35:38 +0700

    feat: add prescript details id in dto

--- src/PillPal.Application/Features/PrescriptDetails/PrescriptDetailDto.cs
+++ src/PillPal.Application/Features/PrescriptDetails/PrescriptDetailDto.cs
@@ -2,6 +2,9 @@
 
 public record PrescriptDetailDto
 {
+    /// <example>00000000-0000-0000-0000-000000000000</example>
+    public Guid Id { get; init; }
+    
     /// <example>Paracetamol</example>
     public string? MedicineName { get; init; }
 
