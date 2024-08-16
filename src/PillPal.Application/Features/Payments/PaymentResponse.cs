bec21cab1ffa92d4dd2e903e8d3e0f478d9ed463
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-08-16 22:59:10 +0700

    docs: xml docs to payment response

--- src/PillPal.Application/Features/Payments/PaymentResponse.cs
+++ src/PillPal.Application/Features/Payments/PaymentResponse.cs
@@ -2,7 +2,12 @@ namespace PillPal.Application.Features.Payments;
 
 public record PaymentResponse
 {
+    /// <example>https://sandbox.monkepay.com/paylink</example>
     public string? PaymentUrl { get; set; }
+
+    /// <example>00000000-0000-0000-0000-000000000000</example>
     public Guid CustomerPackageId { get; init; }
+
+    /// <example>zp-monkepaywithdoofTransToken</example>
     public string? ZpTransToken { get; init; }
 }
