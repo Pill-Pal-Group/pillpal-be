40b0f74daad749bb5318e121496a8a6889a3cace
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-09-24 18:23:59 +0700

    refactor: remove some redundant validation message

--- src/PillPal.Application/Features/Auths/CreatePasswordRequest.cs
+++ src/PillPal.Application/Features/Auths/CreatePasswordRequest.cs
@@ -14,8 +14,8 @@ public class CreatePasswordValidator : AbstractValidator<CreatePasswordRequest>
     public CreatePasswordValidator()
     {
         RuleFor(x => x.Password)
-            .NotEmpty().WithMessage("Password is required.")
-            .MinimumLength(6).WithMessage("Password must be at least 6 characters long.")
+            .NotEmpty()
+            .MinimumLength(6)
             .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
             .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
             .Matches("[0-9]").WithMessage("Password must contain at least one digit.")
